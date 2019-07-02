40e57feb3c4e861136eff46a2525006b740b42be
Sergi Navarro <sergi.navarro@gmail.com>
2019-07-02 23:28:56 +0200

    Admin panel personalised, images are stored in media file and can be seen while editing in DEBUG=TRUE

--- curso-django/personalweb/portfolio/models.py
+++ curso-django/personalweb/portfolio/models.py
@@ -3,6 +3,12 @@ from django.db import models
 class Project(models.Model):
 	title = models.CharField(max_length=200)
 	description = models.TextField()
-	image = models.ImageField()
+	image = models.ImageField(upload_to="projects")
 	created = models.DateTimeField(auto_now_add=True)
 	updated = models.DateTimeField(auto_now=True)
+
+	class Meta:
+		ordering = ["-created"]
+
+	def __str__(self):
+		return self.title 
