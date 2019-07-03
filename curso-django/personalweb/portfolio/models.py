f34cc8051e14d6297a58e95c779e848a88fe597a
Sergi Navarro <sergi.navarro@gmail.com>
2019-07-04 00:36:44 +0200

    Added urlfield to models and modified the template

--- curso-django/personalweb/portfolio/models.py
+++ curso-django/personalweb/portfolio/models.py
@@ -4,8 +4,10 @@ class Project(models.Model):
 	title = models.CharField(max_length=200)
 	description = models.TextField()
 	image = models.ImageField(upload_to="projects")
+	link = models.URLField(null=True, blank=True)
 	created = models.DateTimeField(auto_now_add=True)
 	updated = models.DateTimeField(auto_now=True)
+
 
 	class Meta:
 		ordering = ["-created"]
